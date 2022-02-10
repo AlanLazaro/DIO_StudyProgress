75763877c2b56a73acf0bbd44489eb92c9ef7b3e
alanLazaro <alantsl10@gmail.com>
2022-02-10 14:52:23 -0300

    Calculo de Bhaskara

---
+++ Java/Bhaskara/src/com/dio/Calculo.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+public class Triangulo {
+    protected int ladoA;
+    protected int ladoB;
+    protected int ladoC;
+
+    public Triangulo(int ladoA, int ladoB, int ladoC) {
+        this.ladoA = ladoA;
+        this.ladoB = ladoB;
+        this.ladoC = ladoC;
+
+    }
+}
